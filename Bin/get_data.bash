2476e3bf7715818130eeab1c24d795c334a55f92
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2021-05-06 13:43:34 -0500

    Add script to download data

---
+++ Bin/get_data.bash
@@ -0,0 +1,5 @@
+# Author: Pablo Reyes Moctezuma <info@pabloreyes.com.mx>
+
+# Download data
+# Flag -O (output file)
+wget https://covid.ourworldindata.org/data/owid-covid-data.csv -O Data/covid-data.csv
