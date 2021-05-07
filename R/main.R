0b25e274de5865ab51f986ea8417efbb9b7007d9
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2021-05-07 09:25:24 -0500

    Source functions and themes

--- R/main.R
+++ R/main.R
@@ -16,6 +16,9 @@ caption_data <- str_c("Vaccination Data: Hannah Ritchie _et al._, (2021), _Coron
 caption_world_bank <- str_c("Income Grouping: World Bank (2021), _World Bank Country and Lending Groups_. Published online at DataHelpDesk.WorldBank.org. Retrieved ", Sys.Date(), ".")
 caption_population <- str_c("Population Data: United Nations (2019), _World Population Prospects 2019_. Published online at population.un.org. Retrieved ", Sys.Date(), ".")
 
+# Source functions and themes
+source("R/functions.R")
+source("R/themes.R")
 
 # Load data ====================================================================
 
